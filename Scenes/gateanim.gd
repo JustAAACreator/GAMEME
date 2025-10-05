fa0668720ac479e2ef391d5b41d2f193dfa200f3
Just_A_Creator <Sasha2Likhotvorik@gmail.com>
2025-10-05 12:47:31 +0700

    animations

--- Scenes/gateanim.gd
+++ Scenes/gateanim.gd
@@ -2,7 +2,7 @@ extends Area2D
 @onready var playeranim: AnimationPlayer = $"../Gamemanager/AnimationPlayer"
 @onready var camera_2d_2: Camera2D = $"../Gamemanager/Camera2D2"
 @onready var player: CharacterBody2D = $"../Player"
-
+var endanimfinished = false
 func _on_body_entered(body: CharacterBody2D) -> void:
 	print("anim")
 	body.animationON = true
@@ -12,3 +12,10 @@ func _on_body_entered(body: CharacterBody2D) -> void:
 	camera_2d_2.make_current()
 
 	
+
+
+func _on_animation_player_animation_finished(ending: StringName) -> void:
+	if endanimfinished == false:
+		playeranim.play("uilevelend")
+		endanimfinished = true
+	pass # Replace with function body.
--- gamemanager.gd
+++ gamemanager.gd
@@ -1,7 +1,18 @@
 extends Node
 @onready var score_label: Label = $CanvasLayer/Label
 var score = 0
+const FILE_BEGIN = "res://Scenes/level_"
 
 func add_point():
 	score += 1
 	score_label.text = "STARS : " + str(score)
+
+
+func _on_next_pressed() -> void:
+	var current_scene = get_tree().current_scene.scene_file_path
+	var next_level_number = current_scene.to_int() + 1
+	var next_level = FILE_BEGIN + str(next_level_number) + ".tscn"
+	get_tree().change_scene_to_file(next_level)
+	print(next_level_number)
+	
+	pass # Replace with function body.
